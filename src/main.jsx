5d47cc2da2d69bc721feb6e573c34358b6bb79bf
CODEBOT <pankoustav@gmail.com>
2026-02-10 14:30:58 +0530

    Fix router and base path for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+// CHANGE 1: Import HashRouter instead of BrowserRouter
+import { HashRouter } from 'react-router-dom' 
 import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      {/* CHANGE 2: Wrap App in HashRouter */}
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   </React.StrictMode>,
 )
